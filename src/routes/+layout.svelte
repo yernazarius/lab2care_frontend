d079f46216dde12840fcff2c805f03e498d3d9b4
yernazarius <ernzaryakupov04@gmail.com>
2023-10-23 22:11:08 +0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-3xl font-bold underline">header</h1>
+<p>a content of main +layout.svelte file</p>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+	let wow = data.props;
+	console.log(wow.data);
+</script>
+
+{#each wow.data as { description, id, name }}
+	<p>{description} - {id} - {name}</p>
+{/each}
